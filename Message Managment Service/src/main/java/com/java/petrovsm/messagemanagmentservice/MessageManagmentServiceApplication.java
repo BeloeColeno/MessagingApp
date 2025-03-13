02b69e2cad8c43b52261f13d94df4ec79df66d82
BeloeColeno <semion.semen2013@gmail.com>
2025-03-13 11:38:10 +0500

    Add Message Service

---
+++ Message Managment Service/src/main/java/com/java/petrovsm/messagemanagmentservice/MessageManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java.petrovsm.messagemanagmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageManagmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageManagmentServiceApplication.class, args);
+    }
+
+}
