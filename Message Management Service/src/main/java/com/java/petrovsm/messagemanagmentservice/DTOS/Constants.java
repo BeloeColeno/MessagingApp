43679da76fe8c784ba46038d0c6ffbb79ec978d5
BeloeColeno <semion.semen2013@gmail.com>
2025-03-18 23:01:22 +0500

    Future SQL query for mms.

--- Message Management Service/src/main/java/com/java/petrovsm/messagemanagmentservice/DTOS/Constants.java
+++ Message Management Service/src/main/java/com/java/petrovsm/messagemanagmentservice/DTOS/Constants.java
@@ -17,14 +17,36 @@ public class Constants {
 
     // Database Section
     public static final String DB = "`mms`";
-    public static final String SUBSCRIBERS = "`subscribers`";
-    public static final String PRODUCERS = "`producers`";
-    public static final String MESSAGES = "`messages`";
-    public static final String SUBSCRIPTIONS = "`subscriptions`";
-    public static final String GET_MESSAGE_BY_ID = "";
-    public static final String GET_MESSAGES_FOR_PRODUCER = "";
-    public static final String GET_MESSAGES_FOR_SUBSCRIBER = "";
-    public static final String CREATE_MESSAGE = "";
-    public static final String CREATE_PRODUCER = "";
-    public static final String DELETE_MESSAGE = "";
+    public static final String TABLES_SUBSCRIBERS = "`subscribers`";
+    public static final String TABLES_PRODUCERS = "`producers`";
+    public static final String TABLES_MESSAGES = "`messages`";
+    public static final String TABLES_SUBSCRIPTIONS = "`subscriptions`";
+
+    public static final String GET_MESSAGE_BY_ID = "SELECT * FROM " + TABLES_MESSAGES +
+            "LEFT JOIN " + TABLES_PRODUCERS + " ON " + TABLES_MESSAGES + ".`producer_id` = " + TABLES_PRODUCERS + ".`producer_id` " +
+            "LEFT JOIN " + TABLES_SUBSCRIPTIONS + " ON " + TABLES_PRODUCERS + ".`producer_id` = " + TABLES_SUBSCRIPTIONS + ".`producer_id` " +
+            "LEFT JOIN " + TABLES_SUBSCRIBERS + " ON " + TABLES_SUBSCRIPTIONS + ".`subscriber_id` = " + TABLES_SUBSCRIBERS + ".`subscriber_id` " +
+            "WHERE " + TABLES_MESSAGES + ".`id` = UUID_TO_BIN(?)";
+
+    public static final String GET_MESSAGES_FOR_PRODUCER = "SELECT * FROM " + TABLES_MESSAGES +
+            "LEFT JOIN " + TABLES_PRODUCERS + " ON " + TABLES_MESSAGES + ".`producer_id` = " + TABLES_PRODUCERS + ".`producer_id` " +
+            "LEFT JOIN " + TABLES_SUBSCRIPTIONS + " ON " + TABLES_PRODUCERS + ".`producer_id` = " + TABLES_SUBSCRIPTIONS + ".`producer_id` " +
+            "LEFT JOIN " + TABLES_SUBSCRIBERS + " ON " + TABLES_SUBSCRIPTIONS + ".`subscriber_id` = " + TABLES_SUBSCRIBERS + ".`subscriber_id` " +
+            "WHERE " + TABLES_PRODUCERS + ".`producer_id` = UUID_TO_BIN(?)";
+
+    public static final String GET_MESSAGES_FOR_SUBSCRIBER = "SELECT * FROM " + TABLES_MESSAGES +
+            "LEFT JOIN " + TABLES_PRODUCERS + " ON " + TABLES_MESSAGES + ".`producer_id` = " + TABLES_PRODUCERS + ".`producer_id` " +
+            "LEFT JOIN " + TABLES_SUBSCRIPTIONS + " ON " + TABLES_PRODUCERS + ".`producer_id` = " + TABLES_SUBSCRIPTIONS + ".`producer_id` " +
+            "LEFT JOIN " + TABLES_SUBSCRIBERS + " ON " + TABLES_SUBSCRIPTIONS + ".`subscriber_id` = " + TABLES_SUBSCRIBERS + ".`subscriber_id` " +
+            "WHERE " + TABLES_SUBSCRIBERS + ".`subscriber_id` = UUID_TO_BIN(?)";
+
+    public static final String CREATE_MESSAGE = "INSERT INTO " + TABLES_MESSAGES +
+            " (`id`, `producer_id`, `content`, `created`) VALUES " +
+            "(UUID_TO_BIN(?), UUID_TO_BIN(?), ?, ?)";
+
+    public static final String CREATE_PRODUCER = "INSERT INTO " + TABLES_PRODUCERS +
+            " (`producer_id`) VALUES " +
+            "(UUID_TO_BIN(?))";
+
+    public static final String DELETE_MESSAGE = "DELETE FROM " + TABLES_MESSAGES + " WHERE `id` = UUID_TO_BIN(?)";
 }
