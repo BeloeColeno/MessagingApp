dffe1066057633594434fc0af59e7215aa712cfc
BeloeColeno <semion.semen2013@gmail.com>
2025-03-19 18:44:35 +0500

    Initial commit

---
+++ Subscription Management Service/src/main/java/com/java/petrovsm/subscriptionmanagementservice/SubscriptionManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java.petrovsm.subscriptionmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionManagementServiceApplication.class, args);
+    }
+
+}
---
+++ Subscription Management Service/src/test/java/com/java/petrovsm/subscriptionmanagementservice/SubscriptionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.petrovsm.subscriptionmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
