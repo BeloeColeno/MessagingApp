fe7e172dccc03cb460bb47cfed09da80a21fc227
BeloeColeno <semion.semen2013@gmail.com>
2025-03-19 20:45:18 +0500

    Add Subscription

--- Message Management Service/src/main/java/com/java/petrovsm/messagemanagmentservice/DTOS/Subscription.java
+++ Message Management Service/src/main/java/com/java/petrovsm/messagemanagmentservice/DTOS/Subscription.java
@@ -1,8 +1,21 @@
 package com.java.petrovsm.messagemanagmentservice.DTOS;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subscription {
-    public UUID subscriberId;
-    public UUID producerId;
+    private UUID subscriber;
+    private List<UUID> producers = new ArrayList<>();
+
+    public void addProducer(UUID producerId) {
+        this.producers.add(producerId);
+    }
 }
